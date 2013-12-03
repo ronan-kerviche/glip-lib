4c49b768cea33a5318cd765837cf1f2f438d49da
Ronan Kerviche <ronan-kerviche@users.sf.net>
2013-12-03 01:06:53 -0700

    Missing LibTools file.

---
+++ GLIP-Lib/include/Core/LibTools.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************************************************* */
+/*                                                                                                               */
+/*     GLIP-LIB                                                                                                  */
+/*     OpenGL Image Processing LIBrary                                                                           */
+/*                                                                                                               */
+/*     Author        : R. KERVICHE (ronan.kerviche@free.fr)                                                      */
+/*     LICENSE       : MIT License                                                                               */
+/*     Website       : http://sourceforge.net/projects/glip-lib/                                                 */
+/*                                                                                                               */
+/*     File          : OglInclude.hpp                                                                            */
+/*     Original Date : December 2nd 2013                                                                         */
+/*                                                                                                               */
+/*     Description   : Specific defines for the API structure.                                                   */
+/*                                                                                                               */
+/* ************************************************************************************************************* */
+
+/**
+ * \file    LibTools.hpp
+ * \brief   Specific defines for the API structure.
+ * \author  R. KERVICHE
+ * \date    December 2nd 2013
+**/
+
+#ifndef __LIBTOOLS_INCLUDE__
+#define __LIBTOOLS_INCLUDE__
+
+	#ifdef _WIN32
+		#define	GLIP_API 	__declspec(dllexport)
+		#define	GLIP_API_FUNC 	__declspec(dllexport) extern
+	#else
+		#define GLIP_API 	
+		#define	GLIP_API_FUNC 	extern
+	#endif
+
+#endif
+
